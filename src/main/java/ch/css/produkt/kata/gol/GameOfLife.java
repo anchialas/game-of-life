6fdf75036d227466bc65c291c36e187554601dba
adrian-sommer <adrian.sommer@css.ch>
2022-09-23 10:38:30 +0200

    Added first testcases

--- src/main/java/ch/css/produkt/kata/gol/GameOfLife.java
+++ src/main/java/ch/css/produkt/kata/gol/GameOfLife.java
@@ -6,4 +6,16 @@ public class GameOfLife {
         System.out.println("Hello World!");
     }
 
+    public boolean willBeAlive(int[][] localSpace) {
+        int numberOfAliveNeighbours = 0;
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                if ((x != 1 || y != 1) && localSpace[x][y] != 0) {
+                    ++numberOfAliveNeighbours;
+                }
+            }
+        }
+        return numberOfAliveNeighbours >= 2;
+    }
+
 }
---
+++ src/test/java/ch/css/produkt/kata/gol/GameOfLifeTest.java
@@ -0,0 +1,41 @@
+package ch.css.produkt.kata.gol;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameOfLifeTest {
+    private GameOfLife testee;
+
+    @BeforeEach
+    void setUp() {
+        testee = new GameOfLife();
+    }
+
+    @Test
+    void willBeAlive_tc1() {
+        int[][] world = {
+                {0, 0, 0},
+                {0, 1, 0},
+                {0, 0, 0}
+        };
+
+        boolean result = testee.willBeAlive(world);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void willBeAlive_tc2() {
+        int[][] world = {
+                {0, 0, 0},
+                {1, 1, 1},
+                {0, 0, 0}
+        };
+
+        boolean result = testee.willBeAlive(world);
+
+        assertTrue(result);
+    }
+}
